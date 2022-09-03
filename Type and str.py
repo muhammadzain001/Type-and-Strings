e867ce84fe6f127789bc1f2fc223e8a58b42f3e0
Muhammad Zain <112485536+muhammadzain001@users.noreply.github.com>
2022-09-03 12:46:33 +0300

    Add files via upload

---
+++ Type and str.py
@@ -0,0 +1,46 @@
+# the type function allows you to find
+# out the data type of a piece of data to use it you type whatever you want to know the data type of within
+# its parentheses then it will return back what the data type is.
+
+ex_1 = False
+print(type(ex_1))
+
+ex_2 = 29
+print(type(ex_2))
+
+ex_3 = 2.36778
+print(type(ex_3))
+
+#  the string function converts whatever is put between its parentheses into a string to use it.
+print("----------------------------------------------------------------------")
+ex_4 = str(True)
+print(type(ex_4))
+
+# Escape Sequences
+
+# /t Tab Sequence
+print("this\tis\ta\ttab\tline")
+
+# \n New Line
+print("line one \nline two")
+
+# \' &  \" Double or single quote
+print('"When I said \'immediately\' I meant today!"')
+print("\'Do or do not. There is no try\'")
+
+# \\ back slash
+print("All escape sequences contain a \\.")
+
+print('---------------------------------------------------------------')
+
+#Exercise
+
+var = (2.369)
+print(type(var))
+
+print(str(2.369) + " is a float")
+
+print("Hello , I'm Zain, it\'s nice to meet you")
+
+print("******\n*****\n****\n***\n**\n*")
+print("*******\n ***** \n  ***  \n   *   ")
